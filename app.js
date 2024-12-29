06aad4b0906f74ba2d65eb6031166a552dd3e022
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-12-29 23:22:01 +0545

    setup project directory with routes

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const PORT = 5000 || process.env.PORT;
 
 const expresslayout = require('express-ejs-layouts');
-
-const PORT = 5000 || process.env.PORT;
 
 // setup static folder to public
 app.use(express.static('public'));
@@ -16,9 +15,8 @@ app.use(expresslayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.get('', (req, res)=>{
-  res.send("Hello world");
-})
+
+app.use('/', require('./server/routes/main'));
 
 app.listen(PORT, ()=>{
   console.log(`This app is running in port: ${PORT}.`);
