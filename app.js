c6ccde9bbbc377cf25c25224520bc97cf65e216a
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-12-29 22:50:38 +0545

    setup template engine and public folder

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const express = require('express');
 
 const app = express();
 
+const expresslayout = require('express-ejs-layouts');
+
 const PORT = 5000 || process.env.PORT;
+
+// setup static folder to public
+app.use(express.static('public'));
+
+// Template Engine
+app.use(expresslayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 app.get('', (req, res)=>{
   res.send("Hello world");
